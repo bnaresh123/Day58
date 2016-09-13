e9b2a0a6e8fb7e39c746cbcdcf904fef455997a4
Govardhan <Govardhan>
2016-09-13 10:11:02 +0530

    this is my third commit

---
+++ src/Pack3/TestClass2.java
@@ -0,0 +1,15 @@
+package Pack3;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	@Test
+	public void TestClass2_Method1(){
+		System.out.println("TestClass2_Method1");
+	}
+	@Test
+	public void TestClass2_Method2(){
+		System.out.println("TestClass2_Method2");
+	}
+
+}
